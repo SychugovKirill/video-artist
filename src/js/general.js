aeab1b91ff39265297f2dc895c7af311535135d4
SychugovKirill <9090000640@mail.ru>
2021-05-07 21:26:20 +0500

    делает promo

--- src/js/general.js
+++ src/js/general.js
@@ -1,1 +1,8 @@
-// Silence is gold
+
+const nav = document.querySelector('#header__nav');
+const headerBurger = document.querySelector('#header__burger');
+
+headerBurger.addEventListener("click",  () => {
+  nav.classList.toggle(`header__nav--current`);
+  headerBurger.classList.toggle('burger--current');
+});
